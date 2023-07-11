e4d4c29d7632debae8b86c6ff89ee8e2f2b0d0ba
VÕ MINH CƯỜNG <102784644+vominhcuong2501@users.noreply.github.com>
2023-07-11 15:41:20 +0700

    +Add: life cycle hook

--- src/app/components/child/child.component.ts
+++ src/app/components/child/child.component.ts
@@ -1,9 +1,15 @@
 import {
+  AfterContentChecked,
+  AfterContentInit,
   Component,
+  ContentChild,
+  DoCheck,
+  ElementRef,
   Input,
   OnChanges,
   OnInit,
   SimpleChanges,
+  ViewChild,
 } from '@angular/core';
 
 @Component({
@@ -11,9 +17,12 @@ import {
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.scss'],
 })
-export class ChildComponent implements OnChanges, OnInit {
+export class ChildComponent
+  implements OnChanges, OnInit, DoCheck, AfterContentInit, AfterContentChecked
+{
   @Input() userName = '';
 
+  // chạy đầu tiên nếu có binding dữ liệu và sẽ chạy khi có thay đổi còn k có thì không chạy
   ngOnChanges(changes: SimpleChanges) {
     console.log('ngOnCHanges triggered', changes);
     if (!changes['userName'].isFirstChange()) {
@@ -25,7 +34,25 @@ export class ChildComponent implements OnChanges, OnInit {
     }
   }
 
+  // chạy sau OnChanges và chạy 1 lần duy nhất
   ngOnInit(): void {
     console.log('ngOnInit from the child component');
+  }
+
+  // chạy sau OnChanges và OnInit khi OnChanges thay dổi và nó sẽ chạy kể cả khi binding khong thay đổi giá trị => hạn chế sử dụng
+  ngDoCheck(): void {
+    console.log('ngDoCheck triggered');
+  }
+
+  @ViewChild('wrapper') wrapper!: ElementRef;
+  @ContentChild('contentWrapper') content!: ElementRef;
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit - wrapper', this.wrapper);
+    console.log('ngAfterContentInit - contentWrapper', this.content);
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked triggered');
   }
 }
